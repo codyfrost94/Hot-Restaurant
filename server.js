8afab4b1ca7dd31541d6b305378494067c176dda
HaydenEStern <stern.hayden.e@gmail.com>
2017-11-11 11:11:27 -0800

    add express routing for viewing and making reservations

--- server.js
+++ server.js
@@ -28,6 +28,18 @@ var tables = [];
 //   res.json(newTable);
 // });
 
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "viewtables.html"));
+});
+app.get("/make", function(req, res) {
+  res.sendFile(path.join(__dirname, "newtables.html"));
+});
+
 
 // Starts the server to begin listening
 // =============================================================
